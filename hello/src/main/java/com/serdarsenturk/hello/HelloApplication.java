b7bb61e2dc56dab825df0dc7922fb723b414a31d
Serdar <serdarsenturk@windowslive.con>
2020-09-14 03:15:47 +0300

    Add logback.xml

--- hello/src/main/java/com/serdarsenturk/hello/HelloApplication.java
+++ hello/src/main/java/com/serdarsenturk/hello/HelloApplication.java
@@ -1,14 +1,18 @@
 package com.serdarsenturk.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
 public class HelloApplication {
+	private static final Logger logger = LoggerFactory.getLogger(HelloApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
@@ -16,6 +20,12 @@ public class HelloApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		logger.trace("This is a TRACE message.");
+		logger.debug("This is a DEBUG message.");
+		logger.info("This is an INFO message.");
+		logger.warn("This is a WARN message.");
+		logger.error("You guessed it, an ERROR message.");
+		logger.info("User Landed on MainPage");
 		return String.format("Hello %s!", name);
 	}
 }
