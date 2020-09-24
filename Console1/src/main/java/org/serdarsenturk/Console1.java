d6c89a9c849b56cbaa398229a1ea6e101c4d7cb3
Serdar <serdarsenturk@windowslive.con>
2020-09-24 21:56:01 +0300

    Create Mainweb and Console1 modules

---
+++ Console1/src/main/java/org/serdarsenturk/Console1.java
@@ -0,0 +1,9 @@
+package org.serdarsenturk;
+
+public class Console1
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "I'm console1!" );
+    }
+}
---
+++ Mainweb/src/main/java/org/serdarsenturk/Mainweb.java
@@ -0,0 +1,9 @@
+package org.serdarsenturk;
+
+public class Mainweb
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "I'm Mainweb!" );
+    }
+}
