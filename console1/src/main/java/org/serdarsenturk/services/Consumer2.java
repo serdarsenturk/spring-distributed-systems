f233bdc438d41953a6701a6526cfc3a979d4acc9
Serdar <serdarsenturk@windowslive.con>
2020-10-29 22:21:32 +0300

    Create Consumer2 class
    Created

---
+++ console1/src/main/java/org/serdarsenturk/services/Consumer2.java
@@ -0,0 +1,23 @@
+package org.serdarsenturk.services;
+
+import com.rabbitmq.client.Channel;
+
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeoutException;
+
+public class Consumer2 {
+
+    public static void consumeAndSendMail(Channel channel,String queueName) throws IOException, TimeoutException, NoSuchAlgorithmException, KeyManagementException, URISyntaxException {
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" Mail sent to '" + message + "'");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+    }
+}
