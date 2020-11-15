ee18cd0f41ac4e949d0d398c98e3776ae3d36e3a
Serdar <serdarsenturk@windowslive.con>
2020-11-15 18:44:14 +0300

    Update RedisCacheController

--- hello/src/main/java/com/serdarsenturk/hello/controller/RedisCacheController.java
+++ hello/src/main/java/com/serdarsenturk/hello/controller/RedisCacheController.java
@@ -2,14 +2,11 @@ package com.serdarsenturk.hello.controller;
 
 import com.serdarsenturk.hello.service.RedisCacheService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/cachedPage")
 @RestController
 public class RedisCacheController {
-
     @Autowired
     private RedisCacheService redisCacheService;
     private int counter = 0; // Counter, count to is provide us how many times redis responsed to request
