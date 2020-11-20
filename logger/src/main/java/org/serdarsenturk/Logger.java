ae344874ad24e6ed0a61d56ae6b25e5935ffc18e
Serdar <serdarsenturk@windowslive.con>
2020-11-20 14:23:42 +0300

    Get value in redis server with jedis client

--- logger/src/main/java/org/serdarsenturk/Logger.java
+++ logger/src/main/java/org/serdarsenturk/Logger.java
@@ -1,7 +1,10 @@
 package org.serdarsenturk;
 
+import org.apache.log4j.BasicConfigurator;
 import org.serdarsenturk.services.MessageLog;
 import org.serdarsenturk.services.SendToAdmin;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -11,8 +14,16 @@ import java.util.concurrent.TimeoutException;
 
 public class Logger
 {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Logger.class);
+
     public static void main( String[] args ) throws URISyntaxException, IOException, TimeoutException, NoSuchAlgorithmException, KeyManagementException {
-        MessageLog.messageLogger();
-        SendToAdmin.sendToAdmin();
+
+//        MessageLog.messageLogger();
+//        SendToAdmin.sendToAdmin();
+
+        BasicConfigurator.configure();
+
+        Jedis jedis = new Jedis("localhost", 6379);
+        logger.info("Stored data in cache :" + jedis.get("cache#1"));
     }
 }
