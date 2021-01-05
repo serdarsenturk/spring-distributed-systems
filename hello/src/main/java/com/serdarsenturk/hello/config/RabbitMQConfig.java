f583c22999f3a8b3ec6d60df325b665d4d32ddf1
Serdar <serdarsenturk@windowslive.con>
2021-01-05 21:59:37 +0300

    Mark rabbitTemplates for Qualifier

--- hello/src/main/java/com/serdarsenturk/hello/config/RabbitMQConfig.java
+++ hello/src/main/java/com/serdarsenturk/hello/config/RabbitMQConfig.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -51,6 +52,7 @@ public class RabbitMQConfig {
     }
 
     @Bean
+    @Qualifier
     public AmqpTemplate rabbitTemplates(ConnectionFactory connectionFactory) {
         final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(jsonMessageConverter());
