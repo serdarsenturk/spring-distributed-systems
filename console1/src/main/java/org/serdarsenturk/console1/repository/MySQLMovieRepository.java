65bbe3c450e29fdf5ed0724f6b4dd040ef388830
Serdar <serdarsenturk@windowslive.con>
2021-01-09 17:02:08 +0300

    Update

--- console1/src/main/java/org/serdarsenturk/console1/repository/MySQLMovieRepository.java
+++ console1/src/main/java/org/serdarsenturk/console1/repository/MySQLMovieRepository.java
@@ -1,6 +1,24 @@
+package org.serdarsenturk.console1.repository;
+
+import org.serdarsenturk.console1.entity.Movie;
+import org.serdarsenturk.console1.util.HibernateUtil;
+import org.springframework.data.domain.Pageable;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+public class MySQLMovieRepository implements MovieRepository{
+
     //EntityManager use EntityManagerFactory interface and it's provide connection between Persistence-Unit
     //EntityManager provide CRUD process on persistence.
     EntityManager entityManager = HibernateUtil.getEntityManagerFactory().createEntityManager();
+
+    @Override
+    public List<Movie> findAll(Movie movie) {
+        return null;
+    }
+
     @Override
     public Movie create(Movie movie) {
         entityManager.getTransaction().begin();
@@ -23,3 +41,4 @@
     public Movie update(int id) {
         return null;
     }
+}
