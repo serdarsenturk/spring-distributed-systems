b0ebd01c69584dd757d7d36190533e34f91f05e0
Serdar <serdarsenturk@windowslive.con>
2021-01-09 16:36:25 +0300

    Add Movie's states and methods and appropriate JPA annotations.

--- console1/src/main/java/org/serdarsenturk/console1/entity/Movie.java
+++ console1/src/main/java/org/serdarsenturk/console1/entity/Movie.java
@@ -1,18 +1,76 @@
 package org.serdarsenturk.console1.entity;
-
-import java.util.ArrayList;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.Date;
-import java.util.List;
 
 // Movie Entity class. I'll use this class to serialize and DAO class
-public class Movie{
-    private int id;
-    private String name;
-    private Date releaseDate;
-    private int budget;
-    private int revenues;
-    private List<String> comments = new ArrayList<>();
 
-    // ToDo : Getter-Setters
+@Entity(name = "Movie")
+@Table(name = "Movie")
+public class Movie implements Serializable {
 
+    //Bind (id) Primary key in mysql "Movie" table.
+    @Id
+    @Column(name = "id")
+    public Integer id;
+
+    //Bind to name column in mysql "Movie" table.
+    @Column(name = "name")
+    public String name;
+
+    //Bind to budget column in mysql "Movie" table.
+    @Column(name = "budget")
+    public Integer budget;
+
+    //Bind to revenues column in mysql "Movie" table.
+    @Column(name = "revenues")
+    public Integer revenues;
+
+    //Bind to Name comments in mysql "Movie" table.
+    //TODO : Change comments type String to List
+    @Column(name = "comments")
+    public String comments;
+
+    //Getter-Setters
+    public Integer getId(){
+        return this.id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public Integer getBudget(){
+        return this.budget;
+    }
+
+    public void setBudget(Integer budget){
+        this.budget = budget;
+    }
+
+    public Integer getRevenues(){
+        return this.revenues;
+    }
+
+    public void setRevenues(Integer revenues){
+        this.revenues = revenues;
+    }
+
+    //TODO : Return comments form of Json
+    public String getComments(){
+        return this.comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
 }
